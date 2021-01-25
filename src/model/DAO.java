ed94abdae77366544c4c895d38341f1339f34ef5
Aldo Pereira <aldopereira.gyn@gmail.com>
2021-01-25 17:27:46 -0300

    Projeto de desenvolvimento de uma agenda web.

---
+++ src/model/DAO.java
@@ -0,0 +1,9 @@
+package model;
+
+public class DAO {
+	
+	public static void main(String[] args) {
+		System.out.println("OI");
+	}
+
+}
---
+++ src/model/JavaBeans.java
@@ -0,0 +1,64 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class JavaBeans {
+	
+	@Id
+	private String idcontato;
+	
+	private String nome;
+	private String fone;
+	private String email;
+	
+	
+	public JavaBeans() {
+		super();
+		
+	}
+	
+
+	public JavaBeans(String idcontato, String nome, String fone, String email) {
+		super();
+		this.idcontato = idcontato;
+		this.nome = nome;
+		this.fone = fone;
+		this.email = email;
+	}
+
+
+	public String getIdcontato() {
+		return idcontato;
+	}
+
+	public void setIdcontato(String idcontato) {
+		this.idcontato = idcontato;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getFone() {
+		return fone;
+	}
+
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
